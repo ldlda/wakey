2831864d73169a205ed4aa22c1fd317bab4285d6
lda <lda@ldlda.com>
2026-02-01 19:32:49 +0700

    coolest file ever

---
+++ remote/copy.ps1
@@ -0,0 +1,17 @@
+# this does one thing
+param(
+    [string]$PASSWD
+)
+
+# idk what this does it works like that then thats how it is
+pscp.exe -l root -scp -pw $PASSWD -r 192.168.100.1:/etc/ldlda_help $PSScriptRoot
+
+$initDir = Join-Path $PSScriptRoot 'init.d'
+New-Item -ItemType Directory -Force -Path $initDir | Out-Null
+
+# these are sum
+$files = "update_wakey" , "wakey" , "update_tailscale" , "wireguard_setup"
+$remote = $files.ForEach({ "/etc/init.d/$_" })
+$remote | ForEach-Object {
+    pscp.exe -l root -scp -pw $PASSWD "192.168.100.1:$_" "$initDir\"
+}
